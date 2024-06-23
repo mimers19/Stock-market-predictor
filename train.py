ee846c18d24b92d6ac79164d78999488a1bf5f87
Michal Jankowski <rivivias@gmail.com>
2024-06-23 20:14:06 +0200

    Zmiana pliku pod S3

--- train.py
+++ train.py
@@ -1,17 +1,23 @@
-import os
-import pandas as pd
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, lag
 from pyspark.sql.window import Window
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import RandomForestRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
+import boto3
 
 # Inicjalizacja sesji Spark
 spark = SparkSession.builder.appName('StockPrediction').getOrCreate()
 
-# Ścieżka do folderu z plikami CSV
-data_folder = 's3://my-stock-data-pg-69-2137/'
+# Nazwa bucketu S3
+bucket_name = 'my-stock-data-pg-69-2137'
+
+# Używanie boto3 do uzyskania listy plików w S3
+s3 = boto3.client('s3')
+response = s3.list_objects_v2(Bucket=bucket_name)
+
+# Pobranie wszystkich plików CSV z bucketu
+all_files = [f"s3://{bucket_name}/{item['Key']}" for item in response.get('Contents', []) if item['Key'].endswith('.csv')]
 
 # Funkcja do ładowania i przetwarzania danych z plików CSV
 def load_and_process_data(file_path):
@@ -22,7 +28,6 @@ def load_and_process_data(file_path):
     return df
 
 # Ładowanie danych ze wszystkich plików CSV
-all_files = [os.path.join(data_folder, f) for f in os.listdir(data_folder) if f.endswith('.csv')]
 dataframes = [load_and_process_data(file) for file in all_files]
 data = dataframes[0].unionAll(*dataframes[1:])
 
@@ -33,7 +38,7 @@ data = assembler.transform(data)
 # Podział danych na zestaw treningowy i testowy
 train_data, test_data = data.randomSplit([0.7, 0.3])
 
-
+# Stworzenie modelu Random Forest do regresji
 rf = RandomForestRegressor(labelCol='Close', featuresCol='features')
 model = rf.fit(train_data)
 
@@ -44,4 +49,4 @@ rmse = evaluator.evaluate(predictions)
 print(f"RMSE: {rmse}")
 
 # Zapisanie modelu
-model.save('s3://my-stock-data-pg-69-2137/stock_prediction_model')
+model.save('s3://your-bucket/model/stock_prediction_model')
