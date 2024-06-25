01b8e37987a91bf492d01eb6ff5d961f7bcc8154
Michal Jankowski <rivivias@gmail.com>
2024-06-25 22:31:44 +0200

    drobne poprawy

--- get_data.py
+++ get_data.py
@@ -1,27 +1,34 @@
 import yfinance as yf
 import pandas as pd
 import os
-from tickers import tickers
-# Rozszerzona lista tickerów firm
+import boto3
+from tickers import tickers  # Ensure you have a tickers.py file with a list named tickers
+import io
+
+# AWS S3 Configuration
+s3_bucket = 'my-stock-data-pg-69-2137'
 
 
-# Okres, dla którego pobieramy dane (np. od 2000-01-01 do dziś)
+# Time period for which we retrieve data (e.g., from 2000-01-01 to today)
 start_date = '2000-01-01'
 end_date = '2025-05-30'
 
-# Nazwa folderu do przechowywania plików CSV
-folder_name = 'stock_data'
+# Initialize S3 client
+s3_client = boto3.client('s3')
 
-# Tworzenie folderu, jeśli nie istnieje
-if not os.path.exists(folder_name):
-    os.makedirs(folder_name)
-
-# Pobranie danych i zapisanie do plików CSV
+# Download data and save to S3
 for ticker in tickers:
-    # Pobranie danych historycznych dla wybranej firmy
+    # Download historical data for the selected company
     data = yf.download(ticker, start=start_date, end=end_date)
     
-    # Zapisanie danych do pliku CSV w stworzonym folderze
-    filename = os.path.join(folder_name, f"{ticker}_stock_data.csv")
-    data.to_csv(filename)
-    print(f"Dane dla {ticker} zapisane do pliku {filename}")
+    # Save the data to a CSV file in memory
+    csv_buffer = io.StringIO()
+    data.to_csv(csv_buffer)
+    csv_buffer.seek(0)
+    
+    # Define the S3 object key (file path in the bucket)
+    s3_key = f"stock_data/{ticker}_stock_data.csv"
+    
+    # Upload the CSV file to S3
+    s3_client.put_object(Bucket=s3_bucket, Key=s3_key, Body=csv_buffer.getvalue())
+    print(f"Data for {ticker} saved to S3 bucket {s3_bucket} with key {s3_key}")
